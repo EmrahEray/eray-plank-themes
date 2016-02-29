20f775a899ceae28bbd2873f4fa148cf4b90698a
Emrah Eray <snp358@hotmail.com>
2016-02-29 13:16:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,3 +21,14 @@ if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
     echo
     echo "Done"
 fi
+
+read -p "Do you want to install Eray-Siyah-Cizgi [Y/n]?" yn
+
+if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
+
+    echo "Copying plank theme..."
+    cp -Ri Eray-Siyah-Cizgi /usr/share/plank/themes
+
+    echo
+    echo "Done"
+fi
