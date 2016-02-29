e1b9425769966aaf8c6aa3ff4a964278859f1cd0
Emrah Eray <snp358@hotmail.com>
2016-02-29 12:48:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read -p "Do you want to install Eray-Beyaz [Y/n]?" yn
 if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
 
     echo "Copying plank theme..."
-    cp -Ri Eray-Beyaz $HOME/.local/share/plank/themes
+    cp -Ri Eray-Beyaz $HOME/usr/share/plank/themes
 
     echo
     echo "Done"
@@ -16,7 +16,7 @@ read -p "Do you want to install Eray-Siyah [Y/n]?" yn
 if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
 
     echo "Copying plank theme..."
-    cp -Ri Eray-Siyah $HOME/.local/share/plank/themes
+    cp -Ri Eray-Siyah $HOME/usr/share/plank/themes
 
     echo
     echo "Done"
